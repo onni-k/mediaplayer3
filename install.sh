dc8045c9b08e8b1ccafb8b0b76ed1edbcbc30c5d
onni-k <okumpune@gmail.com>
2026-06-13 18:33:14 +0300

    Replace mediaplayer2 with mediaplayer3 in install.sh

--- install.sh
+++ install.sh
@@ -55,13 +55,13 @@ fi
 echo "installing '${IPK_NAME}' to $E2_HOST..."
 if [ -f "$with_pass" ]; then
 	sshpass -p $E2_PASSWORD ssh $E2_USERNAME@$E2_HOST <<- EOFSSH > /dev/null
-		opkg remove enigma2-plugin-extensions-mediaplayer2
+		opkg remove enigma2-plugin-extensions-mediaplayer3
 		opkg --force-downgrade install /tmp/$IPK_NAME
 		rm /tmp/$IPK_NAME
 	EOFSSH
 else
 	ssh $E2_USERNAME@$E2_HOST <<- EOFSSH > /dev/null
-		opkg remove enigma2-plugin-extensions-mediaplayer2
+		opkg remove enigma2-plugin-extensions-mediaplayer3
 		opkg --force-downgrade install /tmp/$IPK_NAME
 		rm /tmp/$IPK_NAME
 	EOFSSH
