41ed76763728055530c7995626fe845f913b20e4
onni-k <okumpune@gmail.com>
2026-06-14 20:40:28 +0300

    Update settings.py

--- plugin/settings.py
+++ plugin/settings.py
@@ -14,20 +14,20 @@ from Screens.Screen import Screen
 from . import _
 from enigma import eEnv
 
-config.plugins.mediaplayer2 = ConfigSubsection()
-config.plugins.mediaplayer2.subtitles = ConfigSubsection()
-config.plugins.mediaplayer2.repeat = ConfigYesNo(default=False)
-config.plugins.mediaplayer2.savePlaylistOnExit = ConfigYesNo(default=True)
-config.plugins.mediaplayer2.saveDirOnExit = ConfigYesNo(default=False)
-config.plugins.mediaplayer2.defaultDir = ConfigDirectory()
-config.plugins.mediaplayer2.sortPlaylists = ConfigYesNo(default=False)
-config.plugins.mediaplayer2.alwaysHideInfoBar = ConfigYesNo(default=True)
-config.plugins.mediaplayer2.hideInfobarAndClose = ConfigYesNo(default=False)
-config.plugins.mediaplayer2.extensionsMenu = ConfigYesNo(default=False)
-config.plugins.mediaplayer2.mainMenu = ConfigYesNo(default=False)
-config.plugins.mediaplayer2.cueSheetForServicemp3 = ConfigOnOff(default=True)
-config.plugins.mediaplayer2.saveLastPosition = ConfigYesNo(default=True)
-config.plugins.mediaplayer2.onMovieStart = ConfigSelection(default = "resume", choices = [
+config.plugins.mediaplayer3 = ConfigSubsection()
+config.plugins.mediaplayer3.subtitles = ConfigSubsection()
+config.plugins.mediaplayer3.repeat = ConfigYesNo(default=False)
+config.plugins.mediaplayer3.savePlaylistOnExit = ConfigYesNo(default=True)
+config.plugins.mediaplayer3.saveDirOnExit = ConfigYesNo(default=False)
+config.plugins.mediaplayer3.defaultDir = ConfigDirectory()
+config.plugins.mediaplayer3.sortPlaylists = ConfigYesNo(default=False)
+config.plugins.mediaplayer3.alwaysHideInfoBar = ConfigYesNo(default=True)
+config.plugins.mediaplayer3.hideInfobarAndClose = ConfigYesNo(default=False)
+config.plugins.mediaplayer3.extensionsMenu = ConfigYesNo(default=False)
+config.plugins.mediaplayer3.mainMenu = ConfigYesNo(default=False)
+config.plugins.mediaplayer3.cueSheetForServicemp3 = ConfigOnOff(default=True)
+config.plugins.mediaplayer3.saveLastPosition = ConfigYesNo(default=True)
+config.plugins.mediaplayer3.onMovieStart = ConfigSelection(default = "resume", choices = [
         ("ask yes", _("Ask user") + " " + _("default") + " " + _("yes")),
         ("ask no", _("Ask user") + " " + _("default") + " " + _("no")),
         ("resume", _("Resume from last position")),
@@ -40,7 +40,7 @@ SERVICE_EXTEPLAYER3 = 5002
 
 LIBMEDIA_CHOICES = {SERVICEMP3:_('Gstreamer')}
 
-config.plugins.mediaplayer2.useLibMedia = ConfigYesNo(default = False)
+config.plugins.mediaplayer3.useLibMedia = ConfigYesNo(default = False)
 fname = "/proc/%d/maps" % os.getpid()
 libMediaTest = False
 
@@ -78,27 +78,27 @@ if (os.path.isfile(eEnv.resolve("$libdir/gstreamer-1.0/libgstdvbvideosinkexp.so"
     os.path.isfile(eEnv.resolve("$libdir/gstreamer-1.0/libgstdvbaudiosinkexp.so"))):
 	sinkChoices.append("experimental")
 
-config.plugins.mediaplayer2.serviceGstPlayer = ConfigSubsection()
-config.plugins.mediaplayer2.serviceGstPlayer.sink = ConfigSelection(default = 'original', choices=sinkChoices)
-config.plugins.mediaplayer2.serviceGstPlayer.subtitles = ConfigYesNo(default=True)
-config.plugins.mediaplayer2.serviceGstPlayer.bufferSize = ConfigInteger(8192, (1024, 1024 * 64))
-config.plugins.mediaplayer2.serviceGstPlayer.bufferDuration = ConfigInteger(0, (0, 100))
+config.plugins.mediaplayer3.serviceGstPlayer = ConfigSubsection()
+config.plugins.mediaplayer3.serviceGstPlayer.sink = ConfigSelection(default = 'original', choices=sinkChoices)
+config.plugins.mediaplayer3.serviceGstPlayer.subtitles = ConfigYesNo(default=True)
+config.plugins.mediaplayer3.serviceGstPlayer.bufferSize = ConfigInteger(8192, (1024, 1024 * 64))
+config.plugins.mediaplayer3.serviceGstPlayer.bufferDuration = ConfigInteger(0, (0, 100))
 
 if libMediaTest:
-    if config.plugins.mediaplayer2.useLibMedia.getValue() == False:
-        config.plugins.mediaplayer2.useLibMedia.value = True
-        config.plugins.mediaplayer2.useLibMedia.save()
+    if config.plugins.mediaplayer3.useLibMedia.getValue() == False:
+        config.plugins.mediaplayer3.useLibMedia.value = True
+        config.plugins.mediaplayer3.useLibMedia.save()
 else:
-    if config.plugins.mediaplayer2.useLibMedia.getValue() == True:
-        config.plugins.mediaplayer2.useLibMedia.value = False
-        config.plugins.mediaplayer2.useLibMedia.save()
+    if config.plugins.mediaplayer3.useLibMedia.getValue() == True:
+        config.plugins.mediaplayer3.useLibMedia.value = False
+        config.plugins.mediaplayer3.useLibMedia.save()
 
-config.plugins.mediaplayer2.libMedia = ConfigSelection(default=str(SERVICEMP3), choices=[(str(k), v) for k,v in LIBMEDIA_CHOICES.items()])
-config.plugins.mediaplayer2.lcdOnVideoPlayback = ConfigSelection(default='default', choices=[
+config.plugins.mediaplayer3.libMedia = ConfigSelection(default=str(SERVICEMP3), choices=[(str(k), v) for k,v in list(LIBMEDIA_CHOICES.items())])
+config.plugins.mediaplayer3.lcdOnVideoPlayback = ConfigSelection(default='default', choices=[
         ('default', _("Default")),
         ('remaining', _("shows remaining time")),
         ('position', _("shows current position"))])
-config.plugins.mediaplayer2.contextMenuType = ConfigSelection(default='intermediate', choices=[
+config.plugins.mediaplayer3.contextMenuType = ConfigSelection(default='intermediate', choices=[
         ("simple", _("Simple")),
         ("intermediate", _("Intermediate")),
         ("expert", _("Expert")) ])
@@ -108,17 +108,17 @@ try:
     import sqlite3
 except ImportError:
     sqlite3 = None
-    config.plugins.mediaplayer2.cueSheetForServicemp3.value = False
+    config.plugins.mediaplayer3.cueSheetForServicemp3.value = False
 
 
 def ServiceGstPlayerApplySettings():
-    if config.plugins.mediaplayer2.serviceGstPlayer.sink.value == "original":
+    if config.plugins.mediaplayer3.serviceGstPlayer.sink.value == "original":
         videoSink, audioSink = ("dvbvideosink", "dvbaudiosink")
     else:
         videoSink, audioSink = ("dvbvideosinkexp", "dvbaudiosinkexp")
-    subtitleEnabled = config.plugins.mediaplayer2.serviceGstPlayer.subtitles.value
-    bufferSize = config.plugins.mediaplayer2.serviceGstPlayer.bufferSize.value
-    bufferDuration = config.plugins.mediaplayer2.serviceGstPlayer.bufferDuration.value
+    subtitleEnabled = config.plugins.mediaplayer3.serviceGstPlayer.subtitles.value
+    bufferSize = config.plugins.mediaplayer3.serviceGstPlayer.bufferSize.value
+    bufferDuration = config.plugins.mediaplayer3.serviceGstPlayer.bufferDuration.value
     serviceapp_client.setGstreamerPlayerSettings(serviceapp_client.OPTIONS_USER, videoSink, audioSink, subtitleEnabled, bufferSize, bufferDuration)
     serviceapp_client.setUseUserSettings()
 
@@ -180,8 +180,8 @@ class MediaPlayerSettings(Screen,ConfigListScreen):
         self.parent = parent
         self.removeAllPositionsCfg = ConfigNothing()
         self.initConfigList()
-        config.plugins.mediaplayer2.saveDirOnExit.addNotifier(self.initConfigList)
-        config.plugins.mediaplayer2.libMedia.addNotifier(self.initConfigList)
+        config.plugins.mediaplayer3.saveDirOnExit.addNotifier(self.initConfigList)
+        config.plugins.mediaplayer3.libMedia.addNotifier(self.initConfigList)
 
         self["setupActions"] = ActionMap(["SetupActions", "ColorActions"],
         {
@@ -196,36 +196,36 @@ class MediaPlayerSettings(Screen,ConfigListScreen):
         self.setTitle(self.setup_title)
 
     def removeNotifiers(self):
-        config.plugins.mediaplayer2.libMedia.notifiers.remove(self.initConfigList)
+        config.plugins.mediaplayer3.libMedia.notifiers.remove(self.initConfigList)
 
     def initConfigList(self, element=None):
         try:
             self.list = []
-            self.list.append(getConfigListEntry(_("context menu"), config.plugins.mediaplayer2.contextMenuType))
-            self.list.append(getConfigListEntry(_("repeat playlist"), config.plugins.mediaplayer2.repeat))
-            self.list.append(getConfigListEntry(_("save playlist on exit"), config.plugins.mediaplayer2.savePlaylistOnExit))
-            self.list.append(getConfigListEntry(_("save last directory on exit"), config.plugins.mediaplayer2.saveDirOnExit))
-            if not config.plugins.mediaplayer2.saveDirOnExit.getValue():
-                self.list.append(getConfigListEntry(_("start directory"), config.plugins.mediaplayer2.defaultDir))
-            self.list.append(getConfigListEntry(_("sorting of playlists"), config.plugins.mediaplayer2.sortPlaylists))
-            self.list.append(getConfigListEntry(_("always hide infobar"), config.plugins.mediaplayer2.alwaysHideInfoBar))
-            self.list.append(getConfigListEntry(_("hide infobar and close"), config.plugins.mediaplayer2.hideInfobarAndClose))
+            self.list.append(getConfigListEntry(_("context menu"), config.plugins.mediaplayer3.contextMenuType))
+            self.list.append(getConfigListEntry(_("repeat playlist"), config.plugins.mediaplayer3.repeat))
+            self.list.append(getConfigListEntry(_("save playlist on exit"), config.plugins.mediaplayer3.savePlaylistOnExit))
+            self.list.append(getConfigListEntry(_("save last directory on exit"), config.plugins.mediaplayer3.saveDirOnExit))
+            if not config.plugins.mediaplayer3.saveDirOnExit.getValue():
+                self.list.append(getConfigListEntry(_("start directory"), config.plugins.mediaplayer3.defaultDir))
+            self.list.append(getConfigListEntry(_("sorting of playlists"), config.plugins.mediaplayer3.sortPlaylists))
+            self.list.append(getConfigListEntry(_("always hide infobar"), config.plugins.mediaplayer3.alwaysHideInfoBar))
+            self.list.append(getConfigListEntry(_("hide infobar and close"), config.plugins.mediaplayer3.hideInfobarAndClose))
             if sqlite3 is None:
                 self.list.append(getConfigListEntry(_("sqlite3 library is missing!, cuesheets for servicemp3 disabled"), ConfigNothing()))
             else:
-                self.list.append(getConfigListEntry(_("cuesheets for servicemp3 (restart plugin)"), config.plugins.mediaplayer2.cueSheetForServicemp3))
-                if config.plugins.mediaplayer2.cueSheetForServicemp3.value:
+                self.list.append(getConfigListEntry(_("cuesheets for servicemp3 (restart plugin)"), config.plugins.mediaplayer3.cueSheetForServicemp3))
+                if config.plugins.mediaplayer3.cueSheetForServicemp3.value:
                     self.list.append(getConfigListEntry(_("remove all saved positions"), self.removeAllPositionsCfg))
-            self.list.append(getConfigListEntry(_("save last position (restart plugin)"), config.plugins.mediaplayer2.saveLastPosition))
-            self.list.append(getConfigListEntry(_("on movie start (restart plugin)"), config.plugins.mediaplayer2.onMovieStart))
-            self.list.append(getConfigListEntry(_("LCD on video playback"), config.plugins.mediaplayer2.lcdOnVideoPlayback))
-            self.list.append(getConfigListEntry(_("show in extensions menu"), config.plugins.mediaplayer2.extensionsMenu))
-            self.list.append(getConfigListEntry(_("show in main menu"), config.plugins.mediaplayer2.mainMenu))
-            if config.plugins.mediaplayer2.useLibMedia.getValue() == True:
-                self.list.append(getConfigListEntry(_("media framework"), config.plugins.mediaplayer2.libMedia))
-                if int(config.plugins.mediaplayer2.libMedia.value) == SERVICE_GSTPLAYER:
-                    self.list.append(getConfigListEntry(_("sink"), config.plugins.mediaplayer2.serviceGstPlayer.sink))
-                    self.list.append(getConfigListEntry(_("subtitles"), config.plugins.mediaplayer2.serviceGstPlayer.subtitles))
+            self.list.append(getConfigListEntry(_("save last position (restart plugin)"), config.plugins.mediaplayer3.saveLastPosition))
+            self.list.append(getConfigListEntry(_("on movie start (restart plugin)"), config.plugins.mediaplayer3.onMovieStart))
+            self.list.append(getConfigListEntry(_("LCD on video playback"), config.plugins.mediaplayer3.lcdOnVideoPlayback))
+            self.list.append(getConfigListEntry(_("show in extensions menu"), config.plugins.mediaplayer3.extensionsMenu))
+            self.list.append(getConfigListEntry(_("show in main menu"), config.plugins.mediaplayer3.mainMenu))
+            if config.plugins.mediaplayer3.useLibMedia.getValue() == True:
+                self.list.append(getConfigListEntry(_("media framework"), config.plugins.mediaplayer3.libMedia))
+                if int(config.plugins.mediaplayer3.libMedia.value) == SERVICE_GSTPLAYER:
+                    self.list.append(getConfigListEntry(_("sink"), config.plugins.mediaplayer3.serviceGstPlayer.sink))
+                    self.list.append(getConfigListEntry(_("subtitles"), config.plugins.mediaplayer3.serviceGstPlayer.subtitles))
             self["config"].setList(self.list)
         except KeyError:
             print ("keyError")
@@ -235,16 +235,16 @@ class MediaPlayerSettings(Screen,ConfigListScreen):
 
     def keyRight(self):
         ConfigListScreen.keyRight(self)
-        if self["config"].getCurrent()[1] == config.plugins.mediaplayer2.cueSheetForServicemp3:
+        if self["config"].getCurrent()[1] == config.plugins.mediaplayer3.cueSheetForServicemp3:
             self.initConfigList()
 
     def keyLeft(self):
         ConfigListScreen.keyLeft(self)
-        if self["config"].getCurrent()[1] == config.plugins.mediaplayer2.cueSheetForServicemp3:
+        if self["config"].getCurrent()[1] == config.plugins.mediaplayer3.cueSheetForServicemp3:
             self.initConfigList()
 
     def ok(self):
-        if self["config"].getCurrent()[1] == config.plugins.mediaplayer2.defaultDir:
+        if self["config"].getCurrent()[1] == config.plugins.mediaplayer3.defaultDir:
             self.session.openWithCallback(self.DirectoryBrowserClosed, DirectoryBrowser, self.parent.filelist.getCurrentDirectory())
         elif self["config"].getCurrent()[1] == self.removeAllPositionsCfg:
             message = _("Do you really want to delete all saved positions?")
@@ -256,12 +256,12 @@ class MediaPlayerSettings(Screen,ConfigListScreen):
             try:
                 CueSheetDAO.instance.clean_db()
             except Exception as e:
-                print (str(e))
+                print((str(e)))
 
     def DirectoryBrowserClosed(self, path):
-        print("PathBrowserClosed:" + str(path))
+        print(("PathBrowserClosed:" + str(path)))
         if path != False:
-            config.plugins.mediaplayer2.defaultDir.setValue(path)
+            config.plugins.mediaplayer3.defaultDir.setValue(path)
 
     def save(self):
         for x in self["config"].list:
@@ -285,3 +285,4 @@ class MediaPlayerSettings(Screen,ConfigListScreen):
     def createSummary(self):
         from Screens.Setup import SetupSummary
         return SetupSummary
+
